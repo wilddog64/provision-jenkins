0d729f6182805756e99a771b599b2ed1215c84f8
Chengkai Liang <chengkai.liang@dreambox.com>
2017-01-30 08:02:39 -0800

    allow set number of executor and also set jenkins alave port

--- templates/basic-security.groovy
+++ templates/basic-security.groovy
@@ -1,9 +1,17 @@
 #!groovy
+import jenkins.*
+import huson.*
 import hudson.security.*
 import jenkins.model.*
+import com.cloudbees.plugins.credentials.*
+import com.cloudbees.plugins.credentials.common.*
+import com.cloudbees.plugins.credentials.domains.*
+import com.cloudbees.jenkins.plugins.sshcredentials.impl.*
+
 
 def instance = Jenkins.getInstance()
-
+instance.setNumExecutors(5)
+instance.setSlaveAgentPort([55000])
 println "--> Checking if security has been set already"
 
 if (!instance.isUseSecurity()) {
