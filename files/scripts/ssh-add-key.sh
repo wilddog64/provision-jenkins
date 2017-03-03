227b73f70f5b65c99b407e916fa70842959dd67d
Chengkai Liang <chengkai.liang@dreambox.com>
2017-03-02 16:15:44 -0800

    kill ssh-agent if it is running

--- files/scripts/ssh-add-key.sh
+++ files/scripts/ssh-add-key.sh
@@ -3,7 +3,8 @@
 # if ssh-agent is not running then run it
 ssh_passphrase=$1
 ssh_agents=$(pgrep -u jenkins ssh-agent)
-if [[ -z $ssh_agents ]]; then
-	eval "$(ssh-agent -s)"
+if [[ ! -z $ssh_agents ]]; then
+	pkill ssh-agent
 fi
+eval "$(ssh-agent -s)"
 ssh-add
